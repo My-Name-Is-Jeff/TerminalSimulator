dd068722ceca51785665bdfc15d895086e0e93ad
Ishaan Rao <ishaanrao26@gmail.com>
2022-04-17 18:35:14 -0700

    plz work gitignore

--- src/main/kotlin/net/dungeonsim/dungeonsimulator/DungeonSimulator.kt
+++ src/main/kotlin/net/dungeonsim/dungeonsimulator/DungeonSimulator.kt
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class DungeonSimulator : JavaPlugin() {
     override fun onEnable() {
+        // Plugin Startup Logic
     }
     override fun onDisable() {
     }
