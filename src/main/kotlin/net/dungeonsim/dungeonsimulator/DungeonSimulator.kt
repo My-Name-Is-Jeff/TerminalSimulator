cda6628366db2bfe565326d44c899fcd0f9aa201
Ishaan Rao <ishaanrao26@gmail.com>
2022-04-17 18:27:40 -0700

    Basic Infrastructure

---
+++ src/main/kotlin/net/dungeonsim/dungeonsimulator/DungeonSimulator.kt
@@ -0,0 +1,10 @@
+package net.dungeonsim.dungeonsimulator
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DungeonSimulator : JavaPlugin() {
+    override fun onEnable() {
+    }
+    override fun onDisable() {
+    }
+}
